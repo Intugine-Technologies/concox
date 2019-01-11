54d46aa5af13117bb4fe0ee4932a581255902b32
Dirtbox Demon <mayankmittal@intugine.com>
2019-01-11 19:36:46 +0530

    Add device

--- tcp/index.js
+++ tcp/index.js
@@ -25,7 +25,7 @@ server.on('connection', (socket) => {
                     imei = terminals_connected[index].imei;
                 }
             } else {
-                imei = (terminals_connected.find(k => k.remoteAddress === socket.remoteAddress) || {imei: null}).imei;
+                imei = (terminals_connected.find(k => k.remoteAddress === socket.remoteAddress) || { imei: null }).imei;
                 parsed = parsed.map(k => Object.assign({}, k, { imei }));
             }
             data_middleware(parsed);
@@ -44,35 +44,39 @@ server.on('connection', (socket) => {
 });
 
 server.on('error', (err) => {
-    logger.error({event: 'Server error', err});
+    logger.error({ event: 'Server error', err });
 });
 server.on('close', (err) => {
-    logger.log({event: 'Server close', err});
+    logger.log({ event: 'Server close', err });
 });
 
 process.on('unhandledRejection', (reason, promise) => {
-    logger.error({event: 'Unhandled Rejection at:', err: reason.stack.toString() || reason});
+    logger.error({ event: 'Unhandled Rejection at:', err: reason.stack.toString() || reason });
 });
 
 const data_middleware = (data) => {
-    if (data[0].case === '01') send_data_to_api(data);
-    else if (data[0].loc) send_data_to_api(data);
-    else if(data[0].imei) {
-        get_last_loc(data[0].imei)
+    if (data[0].imei) {
+        get_device(data[0].imei)
             .then((r) => {
-                if(r && r.data){
-                    data = data.map(i => Object.assign({}, i, {loc: r.data.loc}));
-                } else {
-                    logger.info({event: 'No Location Available', data});
+                data = data.map(i => Object.assign({}, i, { device: (r && r.data ? r.data.id : 'NA') }));
+                if (data[0].case === '01') send_data_to_api(data);
+                else if (data[0].loc) send_data_to_api(data);
+                else {
+                    get_last_loc(data[0].imei)
+                        .then((r) => {
+                            if (r && r.data) {
+                                data = data.map(i => Object.assign({}, i, { loc: r.data.loc }));
+                            } else {
+                                logger.info({ event: 'No Location Available', data });
+                            }
+                            send_data_to_api(data, (r.data ? r.data.client : null));
+                        }, (e) => {
+                            console.error(e);
+                        });
                 }
-                return get_device(data[0].imei);
-            })
-            .then((r) => {
-                data = data.map(i => Object.assign({}, i, {device: (r && r.data ? r.data.id : 'NA')}));
-                send_data_to_api(data, (r.data ? r.data.client : null));
             })
             .catch((e) => {
-                logger.error({event: 'get_last_loc', data, err: e.response ? e.response.data : e});
+                console.error(e);
             });
     }
     return;
@@ -80,17 +84,17 @@ const data_middleware = (data) => {
 const get_last_loc = (imei) => axios({ url: `${API_BASE}/last_location/${imei}` });
 const get_device = (imei) => axios({ url: `${API_BASE}/device/${imei}` });
 const send_data_to_api = (data, client) => {
-    if(client) mqtt_publisher.publish(client, JSON.stringify(data));
+    if (client) mqtt_publisher.publish(client, JSON.stringify(data));
     axios({
             url: `${API_BASE}/data`,
             method: 'POST',
             data: { data }
         })
         .then((r) => {
-            logger.info({event: 'Sent to API', imei: data[0].imei});
+            logger.info({ event: 'Sent to API', imei: data[0].imei });
         })
         .catch((e) => {
-            logger.error({event: 'Error Sending to API', data, err: e.response.data});
+            logger.error({ event: 'Error Sending to API', data, err: e.response.data });
         });
     return;
 };
@@ -101,13 +105,13 @@ const send_invalid_data_to_api = (data) => {
             data: { data }
         })
         .then((r) => {
-            logger.info({event: 'Invalid Data Sent to API', imei: data[0].imei});
+            logger.info({ event: 'Invalid Data Sent to API', imei: data[0].imei });
         })
         .catch((e) => {
-            logger.error({event: 'Invalid Data Error Sending to API', data, err: e.response.data});
+            logger.error({ event: 'Invalid Data Error Sending to API', data, err: e.response.data });
         });
     return;
 };
 server.listen(config.CONCOX_TCP_PORT, () => {
-    logger.error({event: 'CONCOX_TCP_SERVER STARTED', PORT: config.CONCOX_TCP_PORT});
+    logger.error({ event: 'CONCOX_TCP_SERVER STARTED', PORT: config.CONCOX_TCP_PORT });
 });
