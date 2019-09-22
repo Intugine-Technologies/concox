8c7b32d2812c8065333302f0b24381d8da71475a
Mayank Mittal <mayank_mittal@outlook.com>
2019-09-22 12:29:45 +0530

    Added agentkeepalive to resolve EMFILE error

--- tcp/api_handler.js
+++ tcp/api_handler.js
@@ -1,6 +1,14 @@
 const axios = require('axios');
+const Agent = require('agentkeepalive');
+const keepAliveAgent = new Agent({
+  maxSockets: 100,
+  maxFreeSockets: 10,
+  timeout: 10000,
+  freeSocketTimeout: 30000,
+});
 const config = require("../config");
 module.exports = axios.create({
 	baseURL: `http://127.0.0.1:${config.CONCOX_API_PORT}/concox/`,
-	timeout: 10000
+	timeout: 10000,
+	httpAgent: keepAliveAgent,
 });
