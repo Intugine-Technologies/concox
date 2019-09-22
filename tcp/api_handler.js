13aab837e0e8e72dd8de274a67ad93e1fc813141
Mayank Mittal <mayank_mittal@outlook.com>
2019-09-22 22:38:43 +0530

    Added keepalive true in api handler

--- tcp/api_handler.js
+++ tcp/api_handler.js
@@ -1,14 +1,8 @@
 const axios = require('axios');
-const Agent = require('agentkeepalive');
-const keepAliveAgent = new Agent({
-  maxSockets: 100,
-  maxFreeSockets: 10,
-  timeout: 10000,
-  freeSocketTimeout: 30000,
-});
+const http = require('http');
 const config = require("../config");
 module.exports = axios.create({
 	baseURL: `http://127.0.0.1:${config.CONCOX_API_PORT}/concox/`,
 	timeout: 10000,
-	httpAgent: keepAliveAgent,
+	httpAgent: new http.Agent({keepAlive: true}),
 });
