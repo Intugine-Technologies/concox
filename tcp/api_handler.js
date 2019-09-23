23534bbddab66c95b5e0bd2870d964a7a28f800e
Mayank Mittal <mayank_mittal@outlook.com>
2019-09-23 12:52:17 +0530

    agentkeepalive changed settings

--- tcp/api_handler.js
+++ tcp/api_handler.js
@@ -6,9 +6,9 @@ module.exports = axios.create({
 	baseURL: `http://127.0.0.1:${config.CONCOX_API_PORT}/concox/`,
 	timeout: 10000,
 	httpAgent: new agentkeepalive({
-		maxSockets: 500,
+		maxSockets: 10,
 		maxFreeSockets: 10,
-		timeout: 20,
-		freeSocketTimeout: 10
+		timeout: 10000,
+		freeSocketTimeout: 5000
 	})
 });
