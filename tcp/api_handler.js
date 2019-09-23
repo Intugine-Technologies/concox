33434885e33499dfea4f02e3b4d7ada2480a60a8
Mayank Mittal <mayank_mittal@outlook.com>
2019-09-23 10:54:35 +0530

    agentkeepalive added

--- tcp/api_handler.js
+++ tcp/api_handler.js
@@ -1,8 +1,14 @@
 const axios = require('axios');
 const http = require('http');
+const agentkeepalive = require('agentkeepalive')
 const config = require("../config");
 module.exports = axios.create({
 	baseURL: `http://127.0.0.1:${config.CONCOX_API_PORT}/concox/`,
 	timeout: 10000,
-	httpAgent: new http.Agent({keepAlive: true}),
+	httpAgent: new agentkeepalive({
+		maxSockets: 500,
+		maxFreeSockets: 10,
+		timeout: 20,
+		freeSocketTimeout: 10
+	})
 });
