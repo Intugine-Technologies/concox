5e03d6e9c06d58fa279a46cdd50cee66d7345c2d
mayankmittal <mayankmittal@intugine.com>
2021-08-23 19:44:17 +0530

    fix: add mqtt auth to mqtt publisher

--- mqtt_publisher.js
+++ mqtt_publisher.js
@@ -1,5 +1,9 @@
 const config = require('./config.json');
-const client = require("mqtt").connect(config.BROKER_URL);
+const client = require("mqtt").connect(config.BROKER_URL, {
+    clientId: config.MQTT_CLIENT_ID,
+    username: config.MQTT_USERNAME,
+    password: config.MQTT_PASSWORD
+});
 client.on("connect", function () {
     console.log("Connected to Broker");
 });
